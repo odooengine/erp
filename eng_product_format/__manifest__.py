119007e911ed2b37c19d0edc7767f0d47050b5e5
odooengine <95271859+odooengine@users.noreply.github.com>
2021-12-16 10:45:30 +0500

    Add files via upload

---
+++ eng_product_format/__manifest__.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product Format",
+
+    'summary': """
+        Add New Menus In Products Under Configuration""",
+
+    'description': """
+        Add New Menus In Products Under Configuration
+    """,
+
+    'author': "Viltco",
+    'website': "http://www.viltco.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Stock',
+    'version': '14.0.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'stock'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        'views/age_group_views.xml',
+        'views/calender_season_views.xml',
+        'views/class_views.xml',
+        'views/line_item_views.xml',
+        'views/product_group_views.xml',
+        'views/size_views.xml',
+        'views/product_product_views.xml',
+        'views/product_template_views.xml',
+    ],
+
+}
