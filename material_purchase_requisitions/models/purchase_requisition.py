da67f72cab0a8802d41f367cde559583e55c0678
odooengine <95271859+odooengine@users.noreply.github.com>
2021-12-17 12:00:02 +0500

    Add files via upload

--- material_purchase_requisitions/models/purchase_requisition.py
+++ material_purchase_requisitions/models/purchase_requisition.py
@@ -5,20 +5,18 @@ from odoo import models, fields, api, _
 from datetime import datetime, date
 from odoo.exceptions import Warning, UserError
 
-
 class MaterialPurchaseRequisition(models.Model):
     _name = 'material.purchase.requisition'
     _description = 'Purchase Requisition'
-    # _inherit = ['mail.thread', 'ir.needaction_mixin']
-    _inherit = ['mail.thread', 'mail.activity.mixin', 'portal.mixin']  # odoo11
+    #_inherit = ['mail.thread', 'ir.needaction_mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin', 'portal.mixin']      # odoo11
     _order = 'id desc'
 
-    # @api.multi
+    #@api.multi
     def unlink(self):
         for rec in self:
             if rec.state not in ('draft', 'cancel', 'reject'):
-                raise Warning(
-                    _('You can not delete Purchase Requisition which is not in draft or cancelled or rejected state.'))
+                raise Warning(_('You can not delete Purchase Requisition which is not in draft or cancelled or rejected state.'))
         return super(MaterialPurchaseRequisition, self).unlink()
 
     # name = fields.Char(
@@ -186,9 +184,6 @@ class MaterialPurchaseRequisition(models.Model):
         copy=False,
     )
 
-    vendor_id = fields.Many2one('res.partner')
-    tech_pack = fields.Char('Tech Pack')
-
     @api.model
     def create(self, vals):
         # name = self.env['ir.sequence'].next_by_code('purchase.requisition.seq')
@@ -203,44 +198,42 @@ class MaterialPurchaseRequisition(models.Model):
         # print(res.name)
         return res
 
-    # @api.multi
+    #@api.multi
     def requisition_confirm(self):
         for rec in self:
-            manager_mail_template = self.env.ref(
-                'material_purchase_requisitions.email_confirm_material_purchase_requistion')
+            manager_mail_template = self.env.ref('material_purchase_requisitions.email_confirm_material_purchase_requistion')
             rec.employee_confirm_id = rec.employee_id.id
             rec.confirm_date = fields.Date.today()
             rec.state = 'dept_confirm'
             if manager_mail_template:
                 manager_mail_template.send_mail(self.id)
 
-    # @api.multi
+    #@api.multi
     def requisition_reject(self):
         for rec in self:
             rec.state = 'reject'
             rec.reject_employee_id = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
             rec.userreject_date = fields.Date.today()
 
-    # @api.multi
+    #@api.multi
     def manager_approve(self):
         for rec in self:
             rec.managerapp_date = fields.Date.today()
             rec.approve_manager_id = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
-            employee_mail_template = self.env.ref(
-                'material_purchase_requisitions.email_purchase_requisition_iruser_custom')
+            employee_mail_template = self.env.ref('material_purchase_requisitions.email_purchase_requisition_iruser_custom')
             email_iruser_template = self.env.ref('material_purchase_requisitions.email_purchase_requisition')
             employee_mail_template.sudo().send_mail(self.id)
             email_iruser_template.sudo().send_mail(self.id)
             rec.state = 'ir_approve'
 
-    # @api.multi
+    #@api.multi
     def user_approve(self):
         for rec in self:
             rec.userrapp_date = fields.Date.today()
             rec.approve_employee_id = self.env['hr.employee'].search([('user_id', '=', self.env.uid)], limit=1)
             rec.state = 'approve'
 
-    # @api.multi
+    #@api.multi
     def reset_draft(self):
         for rec in self:
             rec.state = 'draft'
@@ -248,16 +241,16 @@ class MaterialPurchaseRequisition(models.Model):
     @api.model
     def _prepare_pick_vals(self, line=False, stock_id=False):
         pick_vals = {
-            'product_id': line.product_id.id,
-            'product_uom_qty': line.qty,
-            'product_uom': line.uom.id,
-            'location_id': self.location_id.id,
-            'location_dest_id': self.dest_location_id.id,
-            'name': line.product_id.name,
-            'picking_type_id': self.custom_picking_type_id.id,
-            'picking_id': stock_id.id,
-            'custom_requisition_line_id': line.id,
-            'company_id': line.requisition_id.company_id.id,
+            'product_id' : line.product_id.id,
+            'product_uom_qty' : line.qty,
+            'product_uom' : line.uom.id,
+            'location_id' : self.location_id.id,
+            'location_dest_id' : self.dest_location_id.id,
+            'name' : line.product_id.name,
+            'picking_type_id' : self.custom_picking_type_id.id,
+            'picking_id' : stock_id.id,
+            'custom_requisition_line_id' : line.id,
+            'company_id' : line.requisition_id.company_id.id,
         }
         return pick_vals
 
@@ -265,7 +258,7 @@ class MaterialPurchaseRequisition(models.Model):
     def _prepare_po_line(self, line=False, purchase_order=False):
         po_line_vals = {
             'product_id': line.product_id.id,
-            'name': line.product_id.name,
+            'name':line.product_id.name,
             'product_qty': line.qty,
             'product_uom': line.uom.id,
             'date_planned': fields.Date.today(),
@@ -276,12 +269,12 @@ class MaterialPurchaseRequisition(models.Model):
         }
         return po_line_vals
 
-    # @api.multi
+    #@api.multi
     def request_stock(self):
         stock_obj = self.env['stock.picking']
         move_obj = self.env['stock.move']
-        # internal_obj = self.env['stock.picking.type'].search([('code','=', 'internal')], limit=1)
-        # internal_obj = self.env['stock.location'].search([('usage','=', 'internal')], limit=1)
+        #internal_obj = self.env['stock.picking.type'].search([('code','=', 'internal')], limit=1)
+        #internal_obj = self.env['stock.location'].search([('usage','=', 'internal')], limit=1)
         purchase_obj = self.env['purchase.order']
         purchase_line_obj = self.env['purchase.order.line']
         #         if not internal_obj:
@@ -289,7 +282,7 @@ class MaterialPurchaseRequisition(models.Model):
         for rec in self:
             if not rec.requisition_line_ids:
                 raise Warning(_('Please create some requisition lines.'))
-            if any(line.requisition_type == 'internal' for line in rec.requisition_line_ids):
+            if any(line.requisition_type =='internal' for line in rec.requisition_line_ids):
                 if not rec.location_id.id:
                     raise Warning(_('Select Source location under the picking details.'))
                 if not rec.custom_picking_type_id.id:
@@ -299,42 +292,35 @@ class MaterialPurchaseRequisition(models.Model):
                 #                 if not rec.employee_id.dest_location_id.id or not rec.employee_id.department_id.dest_location_id.id:
                 #                     raise Warning(_('Select Destination location under the picking details.'))
                 picking_vals = {
-                    'partner_id': rec.employee_id.sudo().address_home_id.id,
-                    # 'min_date' : fields.Date.today(),
-                    'location_id': rec.location_id.id,
-                    'location_dest_id': rec.dest_location_id and rec.dest_location_id.id or rec.employee_id.dest_location_id.id or rec.employee_id.department_id.dest_location_id.id,
-                    'picking_type_id': rec.custom_picking_type_id.id,  # internal_obj.id,
-                    'note': rec.reason,
-                    'custom_requisition_id': rec.id,
-                    'origin': rec.name,
-                    'company_id': rec.company_id.id,
+                    'partner_id' : rec.employee_id.sudo().address_home_id.id,
+                    #'min_date' : fields.Date.today(),
+                    'location_id' : rec.location_id.id,
+                    'location_dest_id' : rec.dest_location_id and rec.dest_location_id.id or rec.employee_id.dest_location_id.id or rec.employee_id.department_id.dest_location_id.id,
+                    'picking_type_id' : rec.custom_picking_type_id.id,#internal_obj.id,
+                    'note' : rec.reason,
+                    'custom_requisition_id' : rec.id,
+                    'origin' : rec.name,
+                    'company_id' : rec.company_id.id,
 
                 }
                 stock_id = stock_obj.sudo().create(picking_vals)
                 delivery_vals = {
-                    'delivery_picking_id': stock_id.id,
+                    'delivery_picking_id' : stock_id.id,
                 }
                 rec.write(delivery_vals)
 
             po_dict = {}
-            if rec.vendor_id:
-                for line in rec.requisition_line_ids:
-                    line.partner_id = [rec.vendor_id.id]
-            else:
-                raise UserError('Please Select Vendor.')
             for line in rec.requisition_line_ids:
-                if line.requisition_type == 'internal':
+                if line.requisition_type =='internal':
                     pick_vals = rec._prepare_pick_vals(line, stock_id)
                     move_id = move_obj.sudo().create(pick_vals)
-                # else:
-                if line.requisition_type == 'purchase':  # 10/12/2019
+                #else:
+                if line.requisition_type == 'purchase': #10/12/2019
                     if not line.partner_id:
-                        raise Warning(
-                            _('Please enter atleast one vendor on Requisition Lines for Requisition Action Purchase'))
+                        raise Warning(_('Please enter atleast one vendor on Requisition Lines for Requisition Action Purchase'))
 
             rec.action_create_po()
             rec.state = 'stock'
-
     #                     for partner in line.partner_id:
     #                         if partner not in po_dict:
     #                             po_vals = {
@@ -398,7 +384,7 @@ class MaterialPurchaseRequisition(models.Model):
                 # print()
                 if record.requisition_type == 'purchase':
                     if record.product_id.product_tmpl_id.id == res:
-                        val.append((0, 0, {
+                        val.append((0,0, {
                             'product_id': record.product_id.id,
                             # 'product_tmpl_id': record.product_id.product_tmpl_id.id,
                             'name': record.product_id.name,
@@ -419,15 +405,15 @@ class MaterialPurchaseRequisition(models.Model):
                 for i in val:
                     partner = i[2]['partners']
                     i[2].pop('partners')
-                #     for partner in res['partners']:
+            #     for partner in res['partners']:
                 po_vals = {
-                    'partner_id': partner,
-                    'currency_id': self.env.user.company_id.currency_id.id,
-                    'date_order': fields.Date.today(),
-                    'company_id': self.company_id.id,
-                    'order_line': val,
-                    'custom_requisition_id': self.id,
-                    'origin': self.name,
+                            'partner_id': partner,
+                            'currency_id': self.env.user.company_id.currency_id.id,
+                            'date_order': fields.Date.today(),
+                            'company_id': self.company_id.id,
+                            'order_line': val,
+                            'custom_requisition_id': self.id,
+                            'origin': self.name,
                 }
                 purchase_order = self.env['purchase.order'].create(po_vals)
         #     print(val)
@@ -493,13 +479,14 @@ class MaterialPurchaseRequisition(models.Model):
     #                     }
     #                     purchase_order = self.env['purchase.order'].create(po_vals)
 
-    # @api.multi
+
+    #@api.multi
     def action_received(self):
         for rec in self:
             rec.receive_date = fields.Date.today()
             rec.state = 'receive'
 
-    # @api.multi
+    #@api.multi
     def action_cancel(self):
         for rec in self:
             rec.state = 'cancel'
@@ -510,20 +497,19 @@ class MaterialPurchaseRequisition(models.Model):
             rec.department_id = rec.employee_id.sudo().department_id.id
             rec.dest_location_id = rec.employee_id.sudo().dest_location_id.id or rec.employee_id.sudo().department_id.dest_location_id.id
 
-            # @api.multi
-
+            #@api.multi
     def show_picking(self):
         for rec in self:
             res = self.env.ref('stock.action_picking_tree_all')
             res = res.read()[0]
-            res['domain'] = str([('custom_requisition_id', '=', rec.id)])
+            res['domain'] = str([('custom_requisition_id','=',rec.id)])
         return res
 
-    # @api.multi
+    #@api.multi
     def action_show_po(self):
         for rec in self:
             purchase_action = self.env.ref('purchase.purchase_rfq')
             purchase_action = purchase_action.read()[0]
-            purchase_action['domain'] = str([('custom_requisition_id', '=', rec.id)])
+            purchase_action['domain'] = str([('custom_requisition_id','=',rec.id)])
         return purchase_action
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
