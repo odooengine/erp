bfad82adde7b5061afdb8d03a43adc4568deaab9
odooengine <95271859+odooengine@users.noreply.github.com>
2022-01-24 12:46:51 +0500

    Add files via upload

--- manager_all_approvals/models/approval_all_manager.py
+++ manager_all_approvals/models/approval_all_manager.py
@@ -125,7 +125,7 @@ class MRPProductionInh(models.Model):
 
     # review_by_id = fields.Many2one('res.users', string='Reviewed By')
     approve_by_id = fields.Many2one('res.users', string='Approved By')
-
+#
     state = fields.Selection([
         ('draft', 'Draft'),
         ('approve', 'Waiting For Approval'),
@@ -246,7 +246,6 @@ class AccountMoveInh(models.Model):
         self.write({
             'state': 'rejected'
         })
-
 
 
 class AccountPaymentInh(models.Model):
@@ -397,159 +396,6 @@ class AccountPaymentInh(models.Model):
         rec.button_review()
         rec.button_approved()
         print("General entry created")
-
-    # def _prepare_move_line_default_vals(self, write_off_line_vals=None):
-    #     ''' Prepare the dictionary to create the default account.move.lines for the current payment.
-    #     :param write_off_line_vals: Optional dictionary to create a write-off account.move.line easily containing:
-    #         * amount:       The amount to be added to the counterpart amount.
-    #         * name:         The label to set on the line.
-    #         * account_id:   The account on which create the write-off.
-    #     :return: A list of python dictionary to be passed to the account.move.line's 'create' method.
-    #     '''
-    #     print('Helooooooo')
-    #     self.ensure_one()
-    #     write_off_line_vals = write_off_line_vals or {}
-    #
-    #     if not self.journal_id.payment_debit_account_id or not self.journal_id.payment_credit_account_id:
-    #         raise UserError(_(
-    #             "You can't create a new payment without an outstanding payments/receipts account set on the %s journal.",
-    #             self.journal_id.display_name))
-    #
-    #     # Compute amounts.
-    #     write_off_amount = write_off_line_vals.get('amount', 0.0)
-    #
-    #     if self.payment_type == 'inbound':
-    #         # Receive money.
-    #         counterpart_amount = -self.amount
-    #         write_off_amount *= -1
-    #     elif self.payment_type == 'outbound':
-    #         # Send money.
-    #         counterpart_amount = self.amount
-    #     else:
-    #         counterpart_amount = 0.0
-    #         write_off_amount = 0.0
-    #
-    #     balance = self.currency_id._convert(counterpart_amount, self.company_id.currency_id, self.company_id, self.date)
-    #     counterpart_amount_currency = counterpart_amount
-    #     write_off_balance = self.currency_id._convert(write_off_amount, self.company_id.currency_id, self.company_id, self.date)
-    #     write_off_amount_currency = write_off_amount
-    #     currency_id = self.currency_id.id
-    #
-    #     if self.is_internal_transfer:
-    #         if self.payment_type == 'inbound':
-    #             liquidity_line_name = _('Transfer to %s', self.journal_id.name)
-    #         else: # payment.payment_type == 'outbound':
-    #             liquidity_line_name = _('Transfer from %s', self.journal_id.name)
-    #     else:
-    #         liquidity_line_name = self.payment_reference
-    #
-    #     # Compute a default label to set on the journal items.
-    #
-    #     payment_display_name = {
-    #         'outbound-customer': _("Customer Reimbursement"),
-    #         'inbound-customer': _("Customer Payment"),
-    #         'outbound-supplier': _("Vendor Payment"),
-    #         'inbound-supplier': _("Vendor Reimbursement"),
-    #     }
-    #
-    #     default_line_name = self.env['account.move.line']._get_default_line_name(
-    #         _("Internal Transfer") if self.is_internal_transfer else payment_display_name['%s-%s' % (self.payment_type, self.partner_type)],
-    #         self.amount,
-    #         self.currency_id,
-    #         self.date,
-    #         partner=self.partner_id,
-    #     )
-    #
-    #     line_vals_list = [
-    #         # Liquidity line.
-    #         {
-    #             'name': liquidity_line_name or default_line_name,
-    #             'date_maturity': self.date,
-    #             'amount_currency': -counterpart_amount_currency,
-    #             'currency_id': currency_id,
-    #             'debit': balance < 0.0 and -balance or 0.0,
-    #             'credit': balance > 0.0 and balance or 0.0,
-    #             'partner_id': self.partner_id.id,
-    #             'account_id': self.journal_id.payment_debit_account_id.id if balance < 0.0 else self.journal_id.payment_credit_account_id.id,
-    #         },
-    #         # Receivable / Payable.
-    #         {
-    #             'name': self.payment_reference or default_line_name,
-    #             'date_maturity': self.date,
-    #             'amount_currency': counterpart_amount_currency + write_off_amount_currency if currency_id else 0.0,
-    #             'currency_id': currency_id,
-    #             'debit': balance + write_off_balance > 0.0 and balance + write_off_balance or 0.0,
-    #             'credit': balance + write_off_balance < 0.0 and -balance - write_off_balance or 0.0,
-    #             'partner_id': self.partner_id.id,
-    #             'account_id': self.transfer_account_id.id,
-    #         },
-    #
-    #     ]
-    #     if write_off_balance:
-    #         # Write-off line.
-    #         line_vals_list.append({
-    #             'name': write_off_line_vals.get('name') or default_line_name,
-    #             'amount_currency': -write_off_amount_currency,
-    #             'currency_id': currency_id,
-    #             'debit': write_off_balance < 0.0 and -write_off_balance or 0.0,
-    #             'credit': write_off_balance > 0.0 and write_off_balance or 0.0,
-    #             'partner_id': self.partner_id.id,
-    #             'account_id': write_off_line_vals.get('account_id'),
-    #         })
-    #     return line_vals_list
-
-    # def button_approve(self):
-    # AccountMove = self.env['account.move'].with_context(default_type='entry')
-    # for rec in self:
-    #
-    #     if rec.state != 'approve':
-    #         raise UserError(_("Only a draft payment can be posted."))
-    #
-    #     if any(inv.state != 'posted' for inv in rec.invoice_ids):
-    #         raise ValidationError(_("The payment cannot be processed because the invoice is not open!"))
-    #
-    #     # keep the name in case of a payment reset to draft
-    #     if not rec.name:
-    #         # Use the right sequence to set the name
-    #         if rec.payment_type == 'transfer':
-    #             sequence_code = 'account.payment.transfer'
-    #         else:
-    #             if rec.partner_type == 'customer':
-    #                 if rec.payment_type == 'inbound':
-    #                     sequence_code = 'account.payment.customer.invoice'
-    #                 if rec.payment_type == 'outbound':
-    #                     sequence_code = 'account.payment.customer.refund'
-    #             if rec.partner_type == 'supplier':
-    #                 if rec.payment_type == 'inbound':
-    #                     sequence_code = 'account.payment.supplier.refund'
-    #                 if rec.payment_type == 'outbound':
-    #                     sequence_code = 'account.payment.supplier.invoice'
-    #         rec.name = self.env['ir.sequence'].next_by_code(sequence_code, sequence_date=rec.payment_date)
-    #         if not rec.name and rec.payment_type != 'transfer':
-    #             raise UserError(_("You have to define a sequence for %s in your company.") % (sequence_code,))
-    #
-    #     moves = AccountMove.create(rec._prepare_payment_moves())
-    #     moves.filtered(lambda move: move.journal_id.post_at != 'bank_rec').post()
-    #
-    #     # Update the state / move before performing any reconciliation.
-    #     move_name = self._get_move_name_transfer_separator().join(moves.mapped('name'))
-    #     rec.write({'state': 'posted', 'move_name': move_name})
-    #
-    #     if rec.payment_type in ('inbound', 'outbound'):
-    #         # ==== 'inbound' / 'outbound' ====
-    #         if rec.invoice_ids:
-    #             (moves[0] + rec.invoice_ids).line_ids \
-    #                 .filtered(
-    #                 lambda line: not line.reconciled and line.account_id == rec.destination_account_id and not (
-    #                             line.account_id == line.payment_id.writeoff_account_id and line.name == line.payment_id.writeoff_label)) \
-    #                 .reconcile()
-    #     elif rec.payment_type == 'transfer':
-    #         # ==== 'transfer' ====
-    #         moves.mapped('line_ids') \
-    #             .filtered(lambda line: line.account_id == rec.company_id.transfer_account_id) \
-    #             .reconcile()
-    #
-    # return True
 
 
 class StockPickingInh(models.Model):
