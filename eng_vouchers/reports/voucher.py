cbcd2c034b19923eec7ed1138cfee7d7b6cf73c9
odooengine <95271859+odooengine@users.noreply.github.com>
2022-01-25 17:26:16 +0500

    Update voucher.py

--- eng_vouchers/reports/voucher.py
+++ eng_vouchers/reports/voucher.py
@@ -7,14 +7,14 @@ from odoo.exceptions import UserError, ValidationError
 from odoo.tools import float_is_zero, float_compare
 
 
-class EngAccPayment(models.Model):
-    _inherit="account.payment"
+# class EngAccPayment(models.Model):
+#     _inherit="account.payment"
     
     
-    available_partner_bank_ids = fields.Many2many(
-        comodel_name='res.partner.bank',
+#     available_partner_bank_ids = fields.Many2many(
+#         comodel_name='res.partner.bank',
 #         compute='_compute_available_partner_bank_ids',
-    )
+#     )
     
     
 class AccountEdi(models.Model):
