c1236495bf7aea2acf33e9a05bfc9ba2cff39bc8
smcviltco <75363152+smcviltco@users.noreply.github.com>
2022-01-26 11:43:30 +0500

    pushss

--- eng_vouchers/reports/voucher.py
+++ eng_vouchers/reports/voucher.py
@@ -7,9 +7,43 @@ from odoo.exceptions import UserError, ValidationError
 from odoo.tools import float_is_zero, float_compare
 
 
+class EngAccPayment(models.Model):
+    _inherit="account.payment"
+     
+     
+    available_partner_bank_ids = fields.Many2many(
+        comodel_name='res.partner.bank',
+        compute='_compute_available_partner_bank_ids',
+    )
+     
+    
+    @api.depends('partner_id', 'company_id', 'payment_type')
+    def _compute_available_partner_bank_ids(self):
+        for pay in self:
+            if pay.payment_type == 'inbound':
+                pay.available_partner_bank_ids = pay.journal_id.bank_account_id.id
+            else:
+                pay.available_partner_bank_ids = pay.partner_id.bank_ids.filtered(lambda x: x.company_id.id in (False, pay.company_id.id)).id
+#                         .filtered(lambda x: x.company_id.id in (False, pay.company_id.id))._origin
+#                         .filtered(lambda x: x.company_id.id in (False, pay.company_id.id)).id
+  
+class AccountEdi(models.Model):
+    _inherit = 'account.edi.document'
+
+    def action_export_xml(self):
+        pass    
+    
+
+
 class AccountPaymentInherit(models.Model):
     _inherit = 'account.payment'
+    
+    
+  
 
+    
+    
+    
     journal_type = fields.Selection([
         ('sale', 'Sales'),
         ('purchase', 'Purchase'),
