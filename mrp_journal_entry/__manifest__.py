518e4e7fdc28f60543ab000acdd951d9075114d3
odooengine <95271859+odooengine@users.noreply.github.com>
2022-12-14 19:18:07 +0500

    Add files via upload

--- mrp_journal_entry/__manifest__.py
+++ mrp_journal_entry/__manifest__.py
@@ -20,13 +20,12 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base' , 'mrp' , 'account' , 'stock_account'],
+    'depends': ['base' , 'mrp' , 'account'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'security/security.xml',
-        'views/views.xml',
+        # 'views/views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
