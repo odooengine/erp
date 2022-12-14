511292efbdd89107cef06d6608b0d1ac966936f8
odooengine <95271859+odooengine@users.noreply.github.com>
2022-12-14 16:33:24 +0500

    mrp_journal_entry

--- mrp_journal_entry/__manifest__.py
+++ mrp_journal_entry/__manifest__.py
@@ -20,12 +20,13 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base' , 'mrp' , 'account'],
+    'depends': ['base' , 'mrp' , 'account' , 'stock_account'],
 
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        # 'views/views.xml',
+        'security/security.xml',
+        'views/views.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
