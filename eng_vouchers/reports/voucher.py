7f52122e732dfc2ab7fc2a88cba07eadbf8bbf30
odooengine <95271859+odooengine@users.noreply.github.com>
2022-12-19 15:58:29 +0500

    Add files via upload

--- eng_vouchers/reports/voucher.py
+++ eng_vouchers/reports/voucher.py
@@ -9,36 +9,36 @@ from odoo.tools import float_is_zero, float_compare
 # class EngAccPaymentrr(models.TransientModel):
 #     _inherit="account.payment.register"
 #
-#     available_partner_bank_ids = fields.Many2many('res.bank',
+#     available_partner_bank_ids = fields.Many2many('res.partner.bank',
 #
 #     )
 #
-# class EngAccPayment(models.Model):
-#     _inherit="account.payment"
-# 
-#     available_partner_bank_ids = fields.Many2many('res.bank',
-#
-#     )
-#
-#     @api.depends('partner_id', 'company_id', 'payment_type')
-#     def _compute_available_partner_bank_ids(self):
-#         for pay in self:
-#             res_partner_bank_id = self.env['res.bank'].search([],limit=1)[0]
-#             if pay.payment_type == 'inbound':
-#
-#                 pay.available_partner_bank_ids = res_partner_bank_id#pay.journal_id.bank_account_id
-#             else:
-#                 pay.available_partner_bank_ids = res_partner_bank_id
-# #                 pay.partner_id.bank_ids\
-# #                         .filtered(lambda x: x.company_id.id in (False, pay.company_id.id))._origin
-#
-#     @api.depends('available_partner_bank_ids', 'journal_id')
-#     def _compute_partner_bank_id(self):
-#         ''' The default partner_bank_id will be the first available on the partner. '''
-#         for pay in self:
-#             res_partner_bank_id = self.env['res.partner.bank'].search([],limit=1)[0]
-# #             if pay.partner_bank_id not in pay.available_partner_bank_ids._origin:
-#             pay.partner_bank_id = res_partner_bank_id
+class EngAccPayment(models.Model):
+    _inherit="account.payment"
+
+    available_partner_bank_ids = fields.Many2many('res.partner.bank',
+
+    )
+
+    @api.depends('partner_id', 'company_id', 'payment_type')
+    def _compute_available_partner_bank_ids(self):
+        for pay in self:
+            res_partner_bank_id = self.env['res.partner.bank'].search([],limit=1)[0]
+            if pay.payment_type == 'inbound':
+
+                pay.available_partner_bank_ids = res_partner_bank_id#pay.journal_id.bank_account_id
+            else:
+                pay.available_partner_bank_ids = res_partner_bank_id
+#                 pay.partner_id.bank_ids\
+#                         .filtered(lambda x: x.company_id.id in (False, pay.company_id.id))._origin
+
+    @api.depends('available_partner_bank_ids', 'journal_id')
+    def _compute_partner_bank_id(self):
+        ''' The default partner_bank_id will be the first available on the partner. '''
+        for pay in self:
+            res_partner_bank_id = self.env['res.partner.bank'].search([],limit=1)[0]
+#             if pay.partner_bank_id not in pay.available_partner_bank_ids._origin:
+            pay.partner_bank_id = res_partner_bank_id
 
 
 class AccountEdi(models.Model):
