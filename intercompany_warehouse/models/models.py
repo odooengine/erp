fdc8f152818d64c7d3908a354775444571eada41
odooengine <95271859+odooengine@users.noreply.github.com>
2022-12-22 19:18:26 +0500

    Add files via upload

---
+++ intercompany_warehouse/models/models.py
@@ -0,0 +1,45 @@
+from odoo import models, fields, _
+from odoo.exceptions import UserError
+
+
+class PurchaseOrderInh(models.Model):
+    _inherit = 'purchase.order'
+
+    custom_warehouse_id = fields.Many2one('stock.warehouse',)
+
+    def _prepare_sale_order_data(self, name, partner, company, direct_delivery_address):
+        """ Generate the Sales Order values from the PO
+            :param name : the origin client reference
+            :rtype name : string
+            :param partner : the partner reprenseting the company
+            :rtype partner : res.partner record
+            :param company : the company of the created SO
+            :rtype company : res.company record
+            :param direct_delivery_address : the address of the SO
+            :rtype direct_delivery_address : res.partner record
+        """
+        self.ensure_one()
+        print('kkk')
+        partner_addr = partner.sudo().address_get(['invoice', 'delivery', 'contact'])
+        # warehouse = company.warehouse_id and company.warehouse_id.company_id.id == company.id and company.warehouse_id or False
+        warehouse = self.env['stock.warehouse'].search([('company_id', '=', company.id),('code', '=', self.custom_warehouse_id.code)])
+        if not warehouse:
+            raise UserError(_('Configure correct warehouse for company(%s) from Menu: Settings/Users/Companies', company.name))
+        return {
+            'name': self.env['ir.sequence'].sudo().next_by_code('sale.order') or '/',
+            'company_id': company.id,
+            'team_id': self.env['crm.team'].with_context(allowed_company_ids=company.ids)._get_default_team_id(domain=[('company_id', '=', company.id)]).id,
+            'warehouse_id': warehouse.id,
+            'client_order_ref': name,
+            'partner_id': partner.id,
+            'pricelist_id': partner.property_product_pricelist.id,
+            'partner_invoice_id': partner_addr['invoice'],
+            'date_order': self.date_order,
+            'fiscal_position_id': partner.property_account_position_id.id,
+            'payment_term_id': partner.property_payment_term_id.id,
+            'user_id': False,
+            'auto_generated': True,
+            'auto_purchase_order_id': self.id,
+            'partner_shipping_id': direct_delivery_address or partner_addr['delivery'],
+            'order_line': [],
+        }
