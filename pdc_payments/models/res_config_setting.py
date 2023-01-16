528a7771d9b26d3f91b77bc2bf35651d8db92060
odooengine <95271859+odooengine@users.noreply.github.com>
2023-01-16 17:29:20 +0500

    Add files via upload

--- pdc_payments/models/res_config_setting.py
+++ pdc_payments/models/res_config_setting.py
@@ -22,3 +22,25 @@ class ResCompanyInherit(models.Model):
     pdc_receivable = fields.Many2one('account.account', string='PDC Receivable')
     pdc_bnk_vendor = fields.Many2one('account.account', string='PDC Bank Account For Vendor')
     pdc_payable = fields.Many2one('account.account', string='PDC Payable')
+    is_child_company = fields.Boolean()
+    # tax_account_id = fields.Many2one('account.account', string='Tax Account')
+    parent_partner_id = fields.Many2one('res.partner', string='Parent Partner')
+    parent_company_id = fields.Many2one('res.company', string='Parent Company')
+
+    child_lines = fields.One2many('res.company.child', 'company_id')
+
+
+class ResCompanyLine(models.Model):
+    _name = 'res.company.child'
+
+    company_id = fields.Many2one('res.company')
+    # parent_company_id = fields.Many2one('res.company')
+    partner_id = fields.Many2one('res.partner')
+    journal_id = fields.Many2one('account.journal')
+    # child_journal_ids = fields.Many2one('account.journal')
+    child_journal_id = fields.Many2one('account.journal')
+    tax_account_id = fields.Many2one('account.account')
+
+
+
+
--- pdc_payments/wizards/pdc_payment_wizard.py
+++ pdc_payments/wizards/pdc_payment_wizard.py
@@ -22,6 +22,10 @@ class PDCPaymentWizard(models.TransientModel):
     date_payment = fields.Date(string='Payment Date')
     cheque_no = fields.Char()
     move_id = fields.Many2one('account.move', string='Invoice/Bill Ref')
+    is_child = fields.Boolean()
+    is_withholding = fields.Boolean()
+    payment_amount_tax = fields.Float(string='Payment Amount With Tax')
+    analytical_account_id = fields.Many2one('account.analytic.account', string="Operating Unit")
 
     @api.onchange('journal_id')
     def _onchange_journal(self):
@@ -43,7 +47,11 @@ class PDCPaymentWizard(models.TransientModel):
                     'currency_id': record.currency_id.id,
                     'payment_amount': record.payment_amount,
                     'cheque_no': record.cheque_no,
-                    'pdc_type': 'received'
+                    'pdc_type': 'received',
+                    'is_child': record.is_child,
+                    'is_withholding': record.is_withholding,
+                    'payment_amount_tax': record.payment_amount_tax,
+                    'analytical_account_id': record.analytical_account_id.id,
                 }
                 record = self.env['pdc.payment'].create(vals)
             elif rec.move_type == 'in_invoice':
@@ -56,7 +64,11 @@ class PDCPaymentWizard(models.TransientModel):
                     'currency_id': record.currency_id.id,
                     'payment_amount': record.payment_amount,
                     'cheque_no': record.cheque_no,
-                    'pdc_type': 'sent'
+                    'pdc_type': 'sent',
+                    'is_child': record.is_child,
+                    'is_withholding': record.is_withholding,
+                    'payment_amount_tax': record.payment_amount_tax,
+                    'analytical_account_id': record.analytical_account_id.id,
                 }
                 record = self.env['pdc.payment'].create(vals)
 
